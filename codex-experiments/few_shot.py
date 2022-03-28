7b9d46494574c10d80568f920fbcfb81aebb770f
NickSchoelkopf <nick.schoelkopf@yale.edu>
2022-03-27 23:51:39 -0400

    display params at script completion

--- codex-experiments/few_shot.py
+++ codex-experiments/few_shot.py
@@ -10,15 +10,25 @@ from eval_codex import (
 )
 
 print("Loading dataset...\n")
+
+
+n_examples = 50
+mode = "test_few_shot"
+few_shot_n = 10
+k = 80
+
 mathqa_dataset = MathQADataset(
     "/data/lily/nos6/NLP4Code/NLP4Code/data/mathqa/val-python.jsonl",
     "EleutherAI/gpt-neo-125M",
-    50,
-    mode="test_few_shot",
-    few_shot_n=10,
+    n_examples,
+    mode=mode,
+    few_shot_n=few_shot_n,
 )
+print("Dataset loaded.\n")
 
-prompt = "# Answer the following math question:\n"
+
+
+prompt = "# Answer the following math word problem in Python.\n"
 
 
 print("Evaluating...\n")
@@ -30,9 +40,13 @@ programs, acc, pass_at_k = codex_evaluate_pass_at_k(
     mathqa_execution,
     get_ins_few_shot_text=mathqa_ins_few_shot_text,
     get_ins_few_shot_soln=mathqa_ins_few_shot_soln,
-    eval_at_k=80,
+    eval_at_k=k,
     few_shot_enabled=True,
 )
 
-print("Pass@k:", pass_at_k)
+print("Evaluated on {} examples...\n".format(n_examples))
+print("Used {} examples for few-shot evaluation.\n".format(few_shot_n))
+print(k = 80)
+print("Used prompt: \nprompt")
+print(f"Pass@{k}:", pass_at_k)
 print("Accuracy:", acc)
