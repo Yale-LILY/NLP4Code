2997e0174c8a50b8f64003f712f9de29d0296337
haileyschoelkopf <nick.schoelkopf@yale.edu>
2022-07-17 13:25:24 -0400

    make repo pip installable + update readme

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
+
+install_requires=[
+    "transformers@git+https://github.com/huggingface/transformers@main",
+]
 setup(
     name = 'NLP4Code',
     packages = find_packages(),
+    install_requires=install_requires,
 )
