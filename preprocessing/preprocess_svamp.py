78e89379221e15cc5dff5779355cfc0418586d2d
MartinRiddell <martin.riddell@yale.edu>
2022-07-20 19:59:31 -0400

    finished preprocessing of svamp dataset

--- preprocessing/preprocess_svamp.py
+++ preprocessing/preprocess_svamp.py
@@ -1,15 +1,111 @@
 import json
+import os
+import math
 
 from typing import List, Dict, Any
 from csv import DictReader
+from tree_sitter import Language, Parser
 
+# initialize the parser for the code
+language_build_path = os.path.join(os.path.dirname(__file__), 'py-tree-sitter.so')
+PY_LANGUAGE = Language(language_build_path, 'python')
+parser = Parser()
+parser.set_language(PY_LANGUAGE)
 
 input_file = "./data/svamp/svamp.json"
 dev_file = "./data/svamp/svamp.jsonl"
-# keywords = ["ID", "Body", "Question", "Equation", "Answer"]
 
-def get_code_from_answer_str(answer_str: str, question_str: str) -> str:
-    pass
+def get_code_from_answer_str(answer_str: str, equation_str: str) -> str:
+    # reverse_var_dict only keeps the constants and the t_lines does not contain the constant inits
+    reverse_var_dict: Dict[float, str] = {}
+    reverse_temp_var_dict: Dict[float, str] = {}
+    temp_var_num = 0
+    t_lines = []
+
+    def get_var_name(var_str: str, allow_new: bool = True) -> str:
+        num = float(var_str)
+        if num in reverse_temp_var_dict:
+            var_name = reverse_temp_var_dict[num]
+        elif num in reverse_var_dict:
+            var_name = reverse_var_dict[num]
+        elif allow_new:
+            # a new constant
+            var_name = f"n{len(reverse_var_dict)}"
+            reverse_var_dict[num] = var_name
+        else:
+            raise ValueError(f"{var_str} not found in var/temp dict")
+
+        return var_name
+
+    def get_node_text(node, text) -> str:
+        return text[node.start_byte: node.end_byte]
+
+    expression = equation_str
+
+    # interpret the formula with a parse tree
+    assert expression.isascii, f"{expression} is not ascii"
+    parsed_tree = parser.parse(bytes(expression, 'utf-8'))
+
+    # do a dfs on the parsed tree to get the values replaced with names
+    formula_bits = []
+    node_stack = [parsed_tree.root_node.children[0].children[0]]
+    while len(node_stack) > 0:
+        node = node_stack.pop()
+
+        if node.type in ["integer", "float"]:
+            var_name = get_var_name(get_node_text(node, expression))
+            formula_bits.append(var_name)
+        elif node.type in ["+", "-", "*", "/", "**", "(", ")", "//"]:
+            formula_bits.append(get_node_text(node, expression))
+        elif node.type in ["binary_operator", "parenthesized_expression"]:
+            node_stack.extend(node.children[::-1])
+        elif node.type == "unary_operator":
+            if node.children[0].type == "+":
+                var_name = get_var_name(get_node_text(node, expression))
+                formula_bits.append(var_name)
+            elif node.children[0].type == "-":
+                val = -float(get_node_text(node, expression))
+                if val in reverse_temp_var_dict or val in reverse_var_dict:
+                    formula_bits.append(get_var_name(val, allow_new=False))
+                elif -val in reverse_temp_var_dict or val in reverse_var_dict:
+                    formula_bits.append("-"+get_var_name(-val, allow_new=False))
+                else:
+                    formula_bits.append(get_var_name(val, allow_new=True))
+            else:
+                raise ValueError(f"{expression} has unary operator {node.children[0].type}")    
+        else:
+            raise ValueError(f"{expression} has {node.type}")
+
+    right_formula = "".join(formula_bits)
+        
+    # add the temporary var
+    # NOTE: we can't use the len(reverse_temp_var_dict) because we may have the same temp var in different lines
+    temp_var_name = f"t{temp_var_num}"
+    temp_var_num += 1
+    reverse_temp_var_dict[float(answer_str)] = temp_var_name
+
+    # add the line
+    t_lines.append(f"{temp_var_name}={right_formula}")
+
+    # add the const var inits
+    init_lines = []
+    sorted_var_dict = sorted(reverse_var_dict.items(), key=lambda x: int(x[1][1:]))
+    for var_val, var_name in sorted_var_dict:
+        # if the float var is not directly used, and it can be casted as int, do cast as init
+        if not str(var_val) in equation_str and math.isclose(int(var_val), var_val, abs_tol=1e-4):
+            init_lines.append(f"{var_name}={int(var_val)}")
+        else:
+            init_lines.append(f"{var_name}={var_val}")
+
+
+    if len(t_lines) == 0:
+        # no <<formula>> are given for this example, simply skip
+        return "NULL"
+
+    # replace the last line's temp var name with "answer"
+    t_lines[-1] = "answer=" + t_lines[-1].split("=")[1]
+
+    return "\n".join(init_lines + t_lines)
 
 def process_svamp(instances: List[Dict[str, str]]) -> List[Dict[str, Any]]:
     for i, instance in enumerate(instances):
@@ -23,14 +119,15 @@ def process_svamp(instances: List[Dict[str, str]]) -> List[Dict[str, Any]]:
         instance["task_id"] = f"chal_{i}"
         instance.pop("ID")
 
-        instance["original_answer"] = instance["Answer"]
+        instance["answer"] = instance["Answer"]
         instance.pop("Answer")
 
-        instance["code"] = get_code_from_answer_str(instance["Equation"], instance["original_answer"])
+        instance["code"] = get_code_from_answer_str(instance["answer"], instance["Equation"])
+        instance.pop("Equation")
 
         instance.pop("Type")
 
-        print(instance)
+    return instances
 
 if __name__ == "__main__":
     # load the dev data
