0489cb8f06e22c23c408752abdcbc4d5199dc438
troyfeng116 <troy.feng@yale.edu>
2022-10-09 16:08:35 -0400

    Update mathqa reader + update mathqa code T5 finetuning yaml

--- finetuning/lightning_modules/datasets/mathqa_reader.py
+++ finetuning/lightning_modules/datasets/mathqa_reader.py
@@ -4,6 +4,9 @@ from overrides import overrides
 from finetuning.lightning_modules.datasets.base_reader import NL2CodeDataset, NL2CodeDataModule
 
 class MathQADataset(NL2CodeDataset):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     @overrides
     def get_train_instance(self, example: Dict[str, Any]) -> List[Dict[str, Any]]:
@@ -20,16 +23,12 @@ class MathQADataModule(NL2CodeDataModule):
         # OPTIONAL, called for every GPU/machine (assigning state is OK)
         assert stage in ["fit", "validate", "test"]
 
-        train_data = MathQADataset(file_path=self.train_file_path,
-                                   transformer_model_name=self.transformer_model_name,
-                                   max_instances=self.train_max_instances, 
-                                   mask_context_loss=self.mask_context_loss,
-                                   mode="train", few_shot_n=self.few_shot_n)
+        train_data = MathQADataset(transformer_model_name=self.transformer_model_name,
+                                   mode="train",
+                                   **self.train_set_init_args)
         self.train_data = train_data
 
-        val_data = MathQADataset(file_path=self.val_file_path,
-                                 transformer_model_name=self.transformer_model_name,
-                                 max_instances=self.val_max_instances, 
-                                 mask_context_loss=self.mask_context_loss,
-                                 mode="test", few_shot_n=self.few_shot_n)
+        val_data = MathQADataset(transformer_model_name=self.transformer_model_name,
+                                 mode="test",
+                                 **self.val_set_init_args)
         self.val_data = val_data 
