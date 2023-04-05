aa6734988dc51c4643b517b7bf71afb4e6aa557b
troyfeng116 <troy.feng@yale.edu>
2023-04-05 00:54:10 -0400

    try updating PATH in scripts before imports

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,6 +1,11 @@
 import unittest
 
+from os import path
+
+ROOT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
 from execution.executors import MathExecutor
+
 
 class TestDatasets(unittest.TestCase):
     def test_gsmath(self):
--- tests/test_executors.py
+++ tests/test_executors.py
@@ -1,13 +1,20 @@
 import unittest
 
+from os import path
+
+ROOT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
 from execution.executors import MathExecutor
+
 
 class TestExecutors(unittest.TestCase):
     def test_math_executor(self):
         executor = MathExecutor()
 
         test_program = "answer = 5"
-        exec_match, exec_results = executor.exec_program(test_program, {"question": "some question", "answer": 5})
-        
+        exec_match, exec_results = executor.exec_program(
+            test_program, {"question": "some question", "answer": 5}
+        )
+
         self.assertEqual(exec_match, True)
         self.assertEqual(exec_results, {"answer": "5"})
