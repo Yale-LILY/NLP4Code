af3ffa2287c0522318a2f06fa4d8d544f86a29aa
Rui Shen <rs2773@tangra.cs.yale.edu>
2023-06-28 23:21:14 -0400

    added ds1000 prompt file

--- DS_1000/preprocess.py
+++ DS_1000/preprocess.py
@@ -1,5 +1,6 @@
 from ds1000 import DS1000Dataset
 import json
+import random
 
 ds_data = DS1000Dataset("ds1000_data", mode="Completion") # loads all questions into RAM
 
@@ -7,11 +8,6 @@ keys = ["reference_code", "prompt"]
 libraries = ["Matplotlib", "Numpy", "Pandas", "Pytorch", "Scipy", "Sklearn", "Tensorflow"]
 
 metadata_keys = ['lib', 'perturbation_type', 'perturbation_origin_id', 'source_url']
-
-print(ds_data["Numpy"][0]["prompt"])
-is_correct = ds_data["Numpy"][0].test("result = a.shape")
-print(is_correct)
-exit()
 
 for lib in libraries:
     for id, dictionary in enumerate(ds_data[lib]):
@@ -27,3 +23,15 @@ for lib in libraries:
         with open("ds1000.jsonl", 'a') as file:
             json.dump(new_dict, file)
             file.write('\n')
+
+ds1000_jsonl = []
+with open("ds1000.jsonl", 'r') as file:
+        for line in file:
+            json_obj = json.loads(line)
+            ds1000_jsonl.append(json_obj)
+selected_problems = random.sample(ds1000_jsonl, 8)
+for problem in selected_problems:
+     with open("../prompt_files/ds1000-8_exemplars.jsonl", 'a') as file:
+            json.dump(problem, file)
+            file.write('\n')
+
