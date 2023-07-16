27e0e70c0997470cf5842f7cbe6adabb582f1881
rays1024 <raysburnaby@gmail.com>
2023-07-15 22:14:11 -0400

    fixed evaluation error at 158/159th problem

--- DS_1000/preprocess.py
+++ DS_1000/preprocess.py
@@ -1,6 +1,6 @@
-from ds1000 import DS1000Dataset
 import json
 import random
+from ds1000 import DS1000Dataset
 from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("bigcode/starcoder")
@@ -12,41 +12,33 @@ libraries = ["Matplotlib", "Numpy", "Pandas", "Pytorch", "Scipy", "Sklearn", "Te
 
 metadata_keys = ['lib', 'perturbation_type', 'perturbation_origin_id', 'source_url']
 
-with open("../data/ds1000/ds1000.jsonl", "w") as file:
-    file.write("")
+with open("../data/ds1000/ds1000.jsonl", 'w') as file:
+    for lib in libraries:
+        for id, dictionary in enumerate(ds_data[lib]):
+            # if len(tokenizer.tokenize(dictionary['prompt'])) > 2048:
+            #     continue
+            new_dict = {}
+            metadata = {}
+            for key in metadata_keys:
+                metadata[key] = dictionary[key]
+            metadata['id'] = id
+            new_dict["metadata"] = metadata
 
-for lib in libraries:
-    for id, dictionary in enumerate(ds_data[lib]):
-        # if len(tokenizer.tokenize(dictionary['prompt'])) > 2048:
-        #     continue
-        new_dict = {}
-        metadata = {}
-        for key in metadata_keys:
-            metadata[key] = dictionary[key]
-        metadata['id'] = id
-        new_dict["metadata"] = metadata
+            for key in keys:
+                new_dict[key] = dictionary[key]
 
-        for key in keys:
-            new_dict[key] = dictionary[key]
-
-        with open("../data/ds1000/ds1000.jsonl", 'a') as file:
             json.dump(new_dict, file)
             file.write('\n')
 
-
-with open("../prompt_files/ds1000-7_exemplars.jsonl", "w") as file:
-    file.write("")
 selected_problems = []
-last_lib = ""
 with open("../data/ds1000/ds1000.jsonl", 'r') as file:
         for line in file:
             json_obj = json.loads(line)
-            if json_obj["metadata"]["lib"] != last_lib:
-                selected_problems.append(json_obj)
-                last_lib = json_obj["metadata"]["lib"]
+            selected_problems.append(json_obj)
 
-for problem in selected_problems:
-     with open("../prompt_files/ds1000-7_exemplars.jsonl", 'a') as file:
-            json.dump(problem, file)
-            file.write('\n')
+selected_problems = random.sample(selected_problems, 4)
 
+with open("../prompt_files/ds1000-4_exemplars.jsonl", "w") as file:
+    for new_dict in selected_problems:
+        json.dump(new_dict, file)
+        file.write('\n')
